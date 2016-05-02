60162821dca9f1ce03933547e242822cda364380
andreaainara <andrea.ainara10@gmail.com>
2016-05-02 15:35:37 -0700

    I ran out of time :/

--- bog_app/app/controllers/creatures_controller.rb
+++ bog_app/app/controllers/creatures_controller.rb
@@ -3,4 +3,8 @@ class CreaturesController < ApplicationController
     @creatures = Creature.all
     render :index
   end
+
+  def new
+    render :new
+  end
 end
--- bog_app/config/routes.rb
+++ bog_app/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root "creatures#index"
   get "/creatures", to: "creatures#index", as: "creatures"
+  get "/creatures/new", to: "creatures#new"
 end
